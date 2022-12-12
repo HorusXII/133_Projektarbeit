29be60ea3ec05e5d022ad7f7f84b63668bce4f9f
SupportGIBM <40485324+SupportGIBM@users.noreply.github.com>
2022-12-12 10:38:46 +0100

    init

---
+++ js/jqueryayax.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+
+    $.getJSON({
+        dataType: "json",
+        url: "https://gibm.becknet.ch/warenhaus/getFiliale.php?format=JSON",
+        success: function (result) {
+            $.each(result, function (i, value) {
+                item = "<option class='dropdown-item' value='" + value.id + "' >" + value.stadt + ", " + value.strasse + "</option>";
+                $("#dropdown").append(item);
+            });
+
+            if (localStorage.getItem('lastSelectedItem') != null) {
+                $("#dropdown option[value="+ localStorage.getItem('lastSelectedItem') +"]").attr('selected', 'selected');
+            };
+           
+        }
+    });
+
+
+    if (localStorage.getItem('lastSelectedItem') != null) {
+        dataID = localStorage.getItem('lastSelectedItem')
+        ajaxRequest(dataID);
+    };
+
+
+    $("#dropdown").on("change", function () {
+        localStorage.setItem('lastSelectedItem', this.value);     
+        ajaxRequest(this.value)
+    });
+
+
+
+
+    $("#clear").click(function (e) {
+        localStorage.clear();
+    });
+
+
+});
+
+
+function ajaxRequest(value) {
+    if (!$.trim( $('#ausgabe').html() ).length){
+        console.log("empty")
+        $('#ausgabe').append('<table id="tabele" class="table"><tr><th>Stadt</th><th>Strasse</th><th>Öffnungszeit</th></tr></table>');
+    }else{
+        $("#tabele").find("tr:gt(0)").remove();
+    }
+
+    $.getJSON({
+        dataType: "json",
+        url: "https://gibm.becknet.ch/warenhaus/getFiliale.php?filiale=" + value + "&format=JSON",
+        success: function (result) {
+            console.log(result)
+            console.log("success")
+            $('#tabele').append(
+                '<tr><td>' + result[0].stadt +
+                '</td><td>' + result[0].strasse +
+                '</td><td>' + result[0].oeffnungszeiten +
+                '</td></tr>');
+        }
+    });
+}
