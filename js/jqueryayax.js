084384791c65d00a63193d084070803c399832a8
SupportGIBM <40485324+SupportGIBM@users.noreply.github.com>
2022-12-19 11:31:48 +0100

    19.12.22
    Added base calendar
    implemented class selection

--- js/jqueryayax.js
+++ js/jqueryayax.js
@@ -1,34 +1,61 @@
 $(document).ready(function () {
-
-
     $.getJSON({
         dataType: "json",
         url: "http://sandbox.gibm.ch/berufe.php",
         success: function (result) {
-            console.log(result)
-
             $.each(result, function (i, value) {
                 item = "<option class='dropdown-item' value='" + value.beruf_id + "' >" + value.beruf_name + "</option>";
-                $("#dropdown").append(item);
+                $("#berufe").append(item);
             });
-
-            if (localStorage.getItem('lastSelectedItem') != null) {
-                $("#dropdown option[value="+ localStorage.getItem('lastSelectedItem') +"]").attr('selected', 'selected');
-            };
-    
+            if (localStorage.getItem('lastSelectedJob') != null) {
+                $("#berufe option[value="+ localStorage.getItem('lastSelectedJob') +"]").attr('selected', 'selected');
+            };   
         }
     });
 
+    $.getJSON({
+        dataType: "json",
+        url: "http://sandbox.gibm.ch/klassen.php",
+        success: function (result) {
+            $.each(result, function (i, value) {
+                item = "<option class='dropdown-item' value='" + value.klasse_id + "' >" + value.klasse_name + "</option>";
+                $("#klasse").append(item);
+            });
+            if (localStorage.getItem('lastSelectedClass') != null) {
+                $("#klasse option[value="+ localStorage.getItem('lastSelectedClass') +"]").attr('selected', 'selected');
+            };
+        }
+    });
 
-/*     if (localStorage.getItem('lastSelectedItem') != null) {
-        dataID = localStorage.getItem('lastSelectedItem')
-        ajaxRequest(dataID);
-    };
+    
+    $("#berufe").on("change", function () {
+        $("#klasse").empty()
+        localStorage.setItem('lastSelectedJob', this.value);     
+        $.getJSON({
+            dataType: "json",
+            url: "http://sandbox.gibm.ch/klassen.php?beruf_id=" + this.value,
+            success: function (result) {
+                $.each(result, function (i, value) {
+                    item = "<option class='dropdown-item' value='" + value.klasse_id + "' >" + value.klasse_name + "</option>";
+                    $("#klasse").append(item);
+                });
+            }
+        });
+    });
 
-
-    $("#dropdown").on("change", function () {
-        localStorage.setItem('lastSelectedItem', this.value);     
-        ajaxRequest(this.value)
+    $("#klasse").on("change", function () {
+        localStorage.setItem('lastSelectedClass', this.value);     
+        $.getJSON({
+            dataType: "json",
+            url: "http://sandbox.gibm.ch/klassen.php?beruf_id=" + this.value,
+            success: function (result) {
+                $.each(result, function (i, value) {
+                    item = "<option class='dropdown-item' value='" + value.klasse_id + "' >" + value.klasse_name + "</option>";
+                    $("#klasse").append(item);
+                    console.log(value.klasse_name)
+                });
+            }
+        });
     });
 
 
@@ -38,29 +65,4 @@ $(document).ready(function () {
         localStorage.clear();
     });
 
- */
 });
-
-/* 
-function ajaxRequest(value) {
-    if (!$.trim( $('#ausgabe').html() ).length){
-        console.log("empty")
-        $('#ausgabe').append('<table id="tabele" class="table"><tr><th>Stadt</th><th>Strasse</th><th>Öffnungszeit</th></tr></table>');
-    }else{
-        $("#tabele").find("tr:gt(0)").remove();
-    }
-
-    $.getJSON({
-        dataType: "json",
-        url: "https://gibm.becknet.ch/warenhaus/getFiliale.php?filiale=" + value + "&format=JSON",
-        success: function (result) {
-            console.log(result)
-            console.log("success")
-            $('#tabele').append(
-                '<tr><td>' + result[0].stadt +
-                '</td><td>' + result[0].strasse +
-                '</td><td>' + result[0].oeffnungszeiten +
-                '</td></tr>');
-        }
-    });
-} */
